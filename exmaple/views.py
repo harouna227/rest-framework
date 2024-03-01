a09186556e5d9a98459ce0aa8d1b4e23d0292b35
Harouna227 <issifiharouna07@gmail.com>
2024-03-01 15:49:29 +0100

    my theird commit

--- exmaple/views.py
+++ exmaple/views.py
@@ -1,9 +1,12 @@
-from django.http import HttpResponse, JsonResponse
+# from django.http import HttpResponse, JsonResponse
 # from django.views.decorators.csrf import csrf_exempt
 # from rest_framework.parsers import JSONParser
+from django.http import Http404
 
-from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework import mixins, generics
+from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .models import Snippet
@@ -108,4 +111,90 @@ def snippet_detail(request, pk, format=None):
 
     elif request.method == 'DELETE':
         snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)    
+        return Response(status=status.HTTP_204_NO_CONTENT) 
+
+"""****************** Les Vues Basées sur les Classes ********************""" 
+
+# class SnippetList(APIView):
+#     """
+#     Répertoriez tous les extraits de code ou créez un nouvel extrait.
+#     """
+#     def get(self, request, format=None):
+#         snippets = Snippet.objects.all()
+#         serializer = SnippetSerializer(snippets, many=True)
+#         return Response(serializer.data)
+    
+#     def post(self, request, format=None):
+#         serializer = SnippetSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# class SnippetDetail(APIView):
+#     """
+#     Récupérer, mettre à jour ou supprimer un extrait de code.
+#     """
+#     def get_objet(self, pk):
+#         try:
+#             return Snippet.objects.get(pk=pk)
+#         except Snippet.DoesNotExist:
+#             return Http404
+        
+#     def get(self, request, pk, format=None):
+#         snippet = self.get_objet(pk)
+#         serializer = SnippetSerializer(snippet)
+#         return Response(serializer.data)
+    
+#     def put(self, request, pk, format=None):
+#         snippet = self.get_objet(pk)
+#         serializer = SnippetSerializer(snippet, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+#     def delete(self, request, pk, format=None):
+#         snippet = self.get_objet(pk)
+#         snippet.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+"""****************** Les Vues Basées sur les Classes avec les Mixins ********************"""
+
+# class SnippetList(mixins.ListModelMixin,
+#                   mixins.CreateModelMixin,
+#                   generics.GenericAPIView):
+#     queryset = Snippet.objects.all()
+#     serializer_class = SnippetSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+    
+#     def post(self, request, *args, **kwargs):
+#         return self.create(self, request, *args, **kwargs)
+    
+# class SnippetDetail(mixins.RetrieveModelMixin,
+#                     mixins.UpdateModelMixin,
+#                     mixins.DestroyModelMixin,
+#                     generics.GenericAPIView):
+#     queryset = Snippet.objects.all()
+#     serializer_class = SnippetSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+    
+#     def put(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
+    
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)
+    
+"""****************** Les Vues Gérériques basées sur les classes ********************"""
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
